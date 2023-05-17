ce969002e84fee49b55f816530d89d278b71c4eb
Guilherme Souza <guilhermefpv@outlook.com>
2023-05-16 22:55:27 -0300

    install sentry

--- Dockerfile
+++ Dockerfile
@@ -13,29 +13,45 @@ COPY --from=node /usr/lib/ /usr/lib/
 RUN true
 COPY --from=node /usr/local/lib/node_modules /usr/local/lib/node_modules
 COPY requirements requirements
+RUN python -m pip install grpcio
 RUN pip install --no-cache -r requirements/prod.txt
 
 COPY package.json ./
 RUN npm install
 
 COPY webpack.config.js autoapp.py ./
-COPY onlyonecode onlyonecode
+COPY clocktime clocktime
 COPY assets assets
 COPY .env.example .env
 RUN npm run-script build
 
 # ================================= PRODUCTION =================================
 FROM python:${INSTALL_PYTHON_VERSION}-slim-buster as production
-
+RUN apt-get -y update; apt-get -y install curl
+RUN apt-get update \
+  && apt-get install --no-install-recommends --yes \
+    curl \
+    dnsutils \
+    httpie \
+    iputils-ping \
+    jq \
+    netcat-openbsd \
+    net-tools \
+    telnet \
+    vim \
+    wget \
+    && rm -rf /var/lib/apt/lists/*
+    
 WORKDIR /app
+RUN python -m pip install grpcio
+RUN useradd -m sid
+RUN chown -R sid:sid /app
+USER sid
+ENV PATH="/home/sid/.local/bin:${PATH}"
 
-RUN useradd -m onlyonecode
-RUN chown -R onlyonecode:onlyonecode /app
-USER onlyonecode
-ENV PATH="/home/onlyonecode/.local/bin:${PATH}"
-
-COPY --from=builder --chown=onlyonecode:onlyonecode /app/onlyonecode/static /app/onlyonecode/static
+COPY --from=builder --chown=sid:sid /app/clocktime/static /app/clocktime/static
 COPY requirements requirements
+#RUN opentelemetry-bootstrap --action=install
 RUN pip install --no-cache --user -r requirements/prod.txt
 
 COPY supervisord.conf /etc/supervisor/supervisord.conf
@@ -46,6 +62,7 @@ COPY . .
 EXPOSE 5000
 ENTRYPOINT ["/bin/bash", "shell_scripts/supervisord_entrypoint.sh"]
 CMD ["-c", "/etc/supervisor/supervisord.conf"]
+
 
 
 # ================================= DEVELOPMENT ================================
---
+++ clocktime/app.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""The app module, containing the app factory function."""
+import logging
+import logging.handlers
+import sys
+
+import logging_loki
+from multiprocessing import Queue
+from flask import Flask, render_template
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+from settings import DSN_SENTRY
+from opentelemetry.instrumentation.wsgi import OpenTelemetryMiddleware
+from clocktime import commands, public
+from clocktime.extensions import cache, debug_toolbar, flask_static_digest
+
+sentry_sdk.init(
+    dsn=DSN_SENTRY,
+    integrations=[
+        FlaskIntegration(),
+    ],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    # traces_sample_rate=1.0
+    profiles_sample_rate=1.0
+    # Alternatively, to control sampling dynamically
+    # #profiles_sampler=profiles_sampler
+)
+
+
+def create_app(config_object="clocktime.settings"):
+    """Create application factory, as explained here: http://flask.pocoo.org/docs/patterns/appfactories/.
+
+    :param config_object: The configuration object to use.
+    """
+    app = Flask(__name__.split(".")[0])
+    # OpenTelemetry
+    # app.wsgi_app = OpenTelemetryMiddleware(app.wsgi_app)
+    app.config.from_object(config_object)
+    register_extensions(app)
+    register_blueprints(app)
+    register_errorhandlers(app)
+    # register_shellcontext(app)
+    register_commands(app)
+    configure_logger(app)
+    return app
+
+
+def register_extensions(app):
+    """Register Flask extensions."""
+    cache.init_app(app)
+    debug_toolbar.init_app(app)
+    flask_static_digest.init_app(app)
+    return None
+
+
+def register_blueprints(app):
+    """Register Flask blueprints."""
+    app.register_blueprint(public.views.blueprint)
+    return None
+
+
+def register_errorhandlers(app):
+    """Register error handlers."""
+    meter = get_meter_provider().get_meter("clocktime-app", "0.0.1")
+    todo_counter = meter.create_up_down_counter("todo_count")
+
+    def render_error(error):
+        """Render error template."""
+        # If a HTTPException, pull the `code` attribute; default to 500
+        error_code = getattr(error, "code", 500)
+        todo_counter.add(-1)
+        return render_template(f"{error_code}.html"), error_code
+
+    for errcode in [401, 404, 500]:
+        app.errorhandler(errcode)(render_error)
+    return None
+
+
+def register_shellcontext(app):
+    """Register shell context objects."""
+
+    def shell_context():
+        """Shell context objects."""
+
+    app.shell_context_processor(shell_context)
+
+
+def register_commands(app):
+    """Register Click commands."""
+    app.cli.add_command(commands.test)
+    app.cli.add_command(commands.lint)
+
+
+# def configure_logger(app):
+#     """Configure loggers."""
+#     handler = logging.StreamHandler(sys.stdout)
+#     if not app.logger.handlers:
+#         app.logger.addHandler(handler)
+# https://github.com/GreyZmeem/python-logging-loki
+def configure_logger(app):
+    """Configure logging_loki."""
+    handler = logging.StreamHandler(sys.stdout)
+    handler = logging_loki.LokiQueueHandler(
+        Queue(-1),
+        url="https://172./loki/api/v1/push",
+        tags={"application": "clocktime-app"},
+        auth=("username", "password"),
+        version="1",
+    )
+    if not app.logger.handlers:
+        app.logger.addHandler(handler)
